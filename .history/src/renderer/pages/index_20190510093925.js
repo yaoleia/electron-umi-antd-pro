902e8df24a462f2a901a2eb40a421344bff136cf
williamnie <yebeixiaohai@gmail.com>
2019-05-10 09:57:57 +0800

    init

---
+++ .history/src/renderer/pages/index_20190510093925.js
@@ -0,0 +1,16 @@
+
+import React, { PureComponent } from 'react';
+export default class Index extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+
+    return (
+      <div>
+        <p>sdsdsd</p>
+      </div>
+    );
+  }
+}
