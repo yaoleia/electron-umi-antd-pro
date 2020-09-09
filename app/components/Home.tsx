11efb999e96fe12c6c681c7402699625feec6a51
姚磊 <yaolei@ainnovation.com>
2020-09-09 19:01:53 +0800

    dev

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'antd';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
@@ -7,6 +8,9 @@ export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
       <h2>Home</h2>
+      <Button type="text" danger>
+        Button
+      </Button>
       <Link to={routes.COUNTER}>to Counter</Link>
     </div>
   );
