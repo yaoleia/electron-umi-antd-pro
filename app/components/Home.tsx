1121f9f9bac2d192220479bd8b108ae14139df21
姚磊 <yaolei@ainnovation.com>
2020-09-22 16:09:37 +0800

    perf: card渲染，normalize

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Card } from 'antd';
 import { Link } from 'react-router-dom';
 import { findRoute } from '../router';
-import styles from './Home.css';
+import '../style/home.global.scss';
+
+function ProjectItem(props: any) {
+  const { project } = props;
+  return (
+    <Card
+      title={<img src="file:///Users/admin/Desktop/111.jpg" alt="" />}
+      bordered={false}
+    >
+      <p>{project.type}</p>
+      <p>{project.title}</p>
+      <p>{project.description}</p>
+      <p>{project.user}</p>
+      <p>{project.creat}</p>
+    </Card>
+  );
+}
 
 export default function Home(): JSX.Element {
+  const projectData = [
+    {
+      type: 'a',
+      title: '缺陷检测',
+      id: 1,
+      description: 'aaa',
+      user: 'wade',
+      creat: '2020-7-4',
+    },
+    {
+      type: 'b',
+      title: '目标检测',
+      id: 2,
+      description: 'aaa',
+      user: 'wade',
+      creat: '2020-7-4',
+    },
+  ];
+
   const counter = findRoute('counter');
   return (
-    <div className={styles.container} data-tid="container">
+    <div className="home-container" data-tid="container">
+      {projectData.map((project) => (
+        <ProjectItem project={project} key={project.id} />
+      ))}
       <h2>Home</h2>
       <Button type="text" danger>
         Button
