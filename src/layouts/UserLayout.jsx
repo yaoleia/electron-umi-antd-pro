aad7789466a1c455b96f42d5be1fc9640576586a
姚磊 <yaolei@ainnovation.com>
2020-09-29 15:25:58 +0800

    style: 登录页面

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -37,10 +37,9 @@ const UserLayout = (props) => {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>ManuVision</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
           </div>
           {children}
         </div>
