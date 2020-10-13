0cfd04fda3c0dd514a58a94c801569a60267cc0b
姚磊 <yaolei@ainnovation.com>
2020-10-13 17:30:29 +0800

    feat: 增加创建项目Modal

--- src/pages/ProjectList/index.jsx
+++ src/pages/ProjectList/index.jsx
@@ -1,19 +1,17 @@
-import { Card, Col, Form, List, Row, Select, Typography } from 'antd';
-import React, { useEffect } from 'react';
+import { Card, List, Typography, Button, Modal } from 'antd';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'umi';
 import moment from 'moment';
+import ProTable from '@ant-design/pro-table';
 import AvatarList from './components/AvatarList';
-import StandardFormRow from './components/StandardFormRow';
-import TagSelect from './components/TagSelect';
 import styles from './style.less';
 
-const { Option } = Select;
-const FormItem = Form.Item;
 const { Paragraph } = Typography;
 
 const getKey = (id, index) => `${id}-${index}`;
 
 const ProjectList = ({ dispatch, projectList: { list = [] }, loading, history }) => {
+  const [modalVisible, handleModalVisible] = useState(false);
   useEffect(() => {
     dispatch({
       type: 'projectList/fetch',
@@ -80,90 +78,41 @@ const ProjectList = ({ dispatch, projectList: { list = [] }, loading, history })
       )}
     />
   );
-  const formItemLayout = {
-    wrapperCol: {
-      xs: {
-        span: 24,
-      },
-      sm: {
-        span: 16,
-      },
-    },
-  };
   return (
     <div className={styles.coverCardList}>
       <Card bordered={false}>
-        <Form
-          layout="inline"
-          onValuesChange={() => {
-            // 表单项变化时请求数据
-            // 模拟查询表单生效
-            dispatch({
-              type: 'projectList/fetch',
-              payload: {
-                count: 8,
-              },
-            });
-          }}
-        >
-          <StandardFormRow
-            title="所属类目"
-            block
-            style={{
-              paddingBottom: 11,
-            }}
-          >
-            <FormItem name="category">
-              <TagSelect expandable>
-                <TagSelect.Option value="cat1">类目一</TagSelect.Option>
-                <TagSelect.Option value="cat2">类目二</TagSelect.Option>
-                <TagSelect.Option value="cat3">类目三</TagSelect.Option>
-                <TagSelect.Option value="cat4">类目四</TagSelect.Option>
-                <TagSelect.Option value="cat5">类目五</TagSelect.Option>
-                <TagSelect.Option value="cat6">类目六</TagSelect.Option>
-                <TagSelect.Option value="cat7">类目七</TagSelect.Option>
-                <TagSelect.Option value="cat8">类目八</TagSelect.Option>
-                <TagSelect.Option value="cat9">类目九</TagSelect.Option>
-                <TagSelect.Option value="cat10">类目十</TagSelect.Option>
-                <TagSelect.Option value="cat11">类目十一</TagSelect.Option>
-                <TagSelect.Option value="cat12">类目十二</TagSelect.Option>
-              </TagSelect>
-            </FormItem>
-          </StandardFormRow>
-          <StandardFormRow title="其它选项" grid last>
-            <Row gutter={16}>
-              <Col lg={8} md={10} sm={10} xs={24}>
-                <FormItem {...formItemLayout} label="作者" name="author">
-                  <Select
-                    placeholder="不限"
-                    style={{
-                      maxWidth: 200,
-                      width: '100%',
-                    }}
-                  >
-                    <Option value="lisa">王昭君</Option>
-                  </Select>
-                </FormItem>
-              </Col>
-              <Col lg={8} md={10} sm={10} xs={24}>
-                <FormItem {...formItemLayout} label="好评度" name="rate">
-                  <Select
-                    placeholder="不限"
-                    style={{
-                      maxWidth: 200,
-                      width: '100%',
-                    }}
-                  >
-                    <Option value="good">优秀</Option>
-                    <Option value="normal">普通</Option>
-                  </Select>
-                </FormItem>
-              </Col>
-            </Row>
-          </StandardFormRow>
-        </Form>
+        <Button type="primary" onClick={() => handleModalVisible(true)}>
+          新建项目
+        </Button>
       </Card>
       <div className={styles.cardList}>{cardList}</div>
+      <Modal
+        destroyOnClose
+        title="新建项目"
+        visible={modalVisible}
+        onCancel={() => handleModalVisible(false)}
+        footer={null}
+      >
+        <ProTable
+          rowKey="key"
+          type="form"
+          columns={[
+            {
+              title: '规则名称',
+              dataIndex: 'name',
+              tip: '规则名称是唯一的 key',
+              formItemProps: {
+                rules: [
+                  {
+                    required: true,
+                    message: '规则名称为必填项',
+                  },
+                ],
+              },
+            },
+          ]}
+        />
+      </Modal>
     </div>
   );
 };
