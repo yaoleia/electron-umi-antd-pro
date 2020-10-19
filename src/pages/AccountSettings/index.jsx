242a68b8a5b0d14c9ed5326a365f7dad7e7779bd
姚磊 <yaolei@ainnovation.com>
2020-10-19 10:50:27 +0800

    style: 个人设置页

--- src/pages/AccountSettings/index.jsx
+++ src/pages/AccountSettings/index.jsx
@@ -1,16 +1,14 @@
 import { UploadOutlined } from '@ant-design/icons';
-import { Button, Input, Select, Upload, Form, message } from 'antd';
+import { Button, Input, Upload, Form, message } from 'antd';
 import { connect } from 'umi';
 import React, { Component } from 'react';
 import { GridContent } from '@ant-design/pro-layout';
 import GeographicView from './components/GeographicView';
-import PhoneView from './components/PhoneView';
 import styles from './style.less';
-const { Option } = Select; // 头像组件 方便以后独立，增加裁剪之类的功能
 
 const AvatarView = ({ avatar }) => (
   <>
-    <div className={styles.avatar_title}>Avatar</div>
+    <div className={styles.avatar_title}>头像</div>
     <div className={styles.avatar}>
       <img src={avatar} alt="avatar" />
     </div>
@@ -18,7 +16,7 @@ const AvatarView = ({ avatar }) => (
       <div className={styles.button_view}>
         <Button>
           <UploadOutlined />
-          Change avatar
+          上传新头像
         </Button>
       </div>
     </Upload>
@@ -26,29 +24,15 @@ const AvatarView = ({ avatar }) => (
 );
 
 const validatorGeographic = (_, value, callback) => {
-  const { province, city } = value;
+  // const { province, city } = value;
 
-  if (!province.key) {
-    callback('Please input your province!');
-  }
+  // if (!province.key) {
+  //   callback('Please input your province!');
+  // }
 
-  if (!city.key) {
-    callback('Please input your city!');
-  }
-
-  callback();
-};
-
-const validatorPhone = (rule, value, callback) => {
-  const values = value.split('-');
-
-  if (!values[0]) {
-    callback('Please input your area code!');
-  }
-
-  if (!values[1]) {
-    callback('Please input your phone number!');
-  }
+  // if (!city.key) {
+  //   callback('Please input your city!');
+  // }
 
   callback();
 };
@@ -75,7 +59,7 @@ class BaseView extends Component {
     this.view = ref;
   };
   handleFinish = () => {
-    message.success('accountsettings.basic.update.success');
+    message.success('个人设置保存成功！');
   };
 
   render() {
@@ -84,7 +68,7 @@ class BaseView extends Component {
       <GridContent>
         <div className={styles.main}>
           <div className={styles.right}>
-            <div className={styles.title}>Basic Settings</div>
+            <div className={styles.title}>个人设置</div>
             <div className={styles.baseView} ref={this.getViewDom}>
               <div className={styles.left}>
                 <Form
@@ -95,11 +79,11 @@ class BaseView extends Component {
                 >
                   <Form.Item
                     name="email"
-                    label="accountsettings.basic.email"
+                    label="电子邮箱"
                     rules={[
                       {
                         required: true,
-                        message: 'accountsettings.basic.email-message',
+                        message: '请填写电子邮箱地址',
                       },
                     ]}
                   >
@@ -107,56 +91,26 @@ class BaseView extends Component {
                   </Form.Item>
                   <Form.Item
                     name="name"
-                    label="accountsettings.basic.nickname"
+                    label="用户昵称"
                     rules={[
                       {
                         required: true,
-                        message: 'accountsettings.basic.nickname-message',
+                        message: '请填写用户昵称',
                       },
                     ]}
                   >
                     <Input />
                   </Form.Item>
-                  <Form.Item
-                    name="profile"
-                    label="accountsettings.basic.profile"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'accountsettings.basic.profile-message',
-                      },
-                    ]}
-                  >
-                    <Input.TextArea
-                      placeholder="accountsettings.basic.profile-placeholder"
-                      rows={4}
-                    />
-                  </Form.Item>
-                  <Form.Item
-                    name="country"
-                    label="accountsettings.basic.country"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'accountsettings.basic.country-message',
-                      },
-                    ]}
-                  >
-                    <Select
-                      style={{
-                        maxWidth: 220,
-                      }}
-                    >
-                      <Option value="China">中国</Option>
-                    </Select>
+                  <Form.Item name="profile" label="个人简介">
+                    <Input.TextArea rows={4} />
                   </Form.Item>
                   <Form.Item
                     name="geographic"
-                    label="accountsettings.basic.geographic"
+                    label="地区"
                     rules={[
                       {
                         required: true,
-                        message: 'accountsettings.basic.geographic-message',
+                        message: '请选择地区',
                       },
                       {
                         validator: validatorGeographic,
@@ -165,36 +119,9 @@ class BaseView extends Component {
                   >
                     <GeographicView />
                   </Form.Item>
-                  <Form.Item
-                    name="address"
-                    label="accountsettings.basic.address"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'accountsettings.basic.address-message',
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item
-                    name="phone"
-                    label="accountsettings.basic.phone"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'accountsettings.basic.phone-message',
-                      },
-                      {
-                        validator: validatorPhone,
-                      },
-                    ]}
-                  >
-                    <PhoneView />
-                  </Form.Item>
                   <Form.Item>
                     <Button htmlType="submit" type="primary">
-                      Update Information
+                      保存设置
                     </Button>
                   </Form.Item>
                 </Form>
